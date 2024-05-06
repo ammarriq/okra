0671cfeeed0b01c0f88b36253f4e95f2993e68e9
Ammar Iqbal <ammariqbal043@gmail.com>
2024-05-06 16:34:47 +0500

    remove redundant statement

--- src/lib/utils/debounce.ts
+++ src/lib/utils/debounce.ts
@@ -1,15 +1,14 @@
 export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const debounce = <T extends (...args: any[]) => ReturnType<T>>(
-	callback: T,
-	timeout: number
+  callback: T,
+  timeout: number,
 ) => {
-	let timer: ReturnType<typeof setTimeout>
+  let timer: ReturnType<typeof setTimeout>
 
-	return (...args: Parameters<T>) => {
-		clearTimeout(timer)
+  return (...args: Parameters<T>) => {
+    clearTimeout(timer)
 
-		timer = setTimeout(() => callback(...args), timeout)
-	}
+    timer = setTimeout(() => callback(...args), timeout)
+  }
 }
